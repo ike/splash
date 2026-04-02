3999055151ea9bd8a337013a7805d927a7b21492
Isaac Lewis <isaac.lewis@signfracturecare.org>
2026-04-02 11:31:28 -0700

    better webcam save logging

--- get_webcam.sh
+++ get_webcam.sh
@@ -3,11 +3,14 @@
 OUTPUT_HTML="${1:-index.html}"
 OUTPUT_DIR="$(dirname "$OUTPUT_HTML")"
 
-curl -X 'GET' \
+img_url=$(curl -X 'GET' \
   'https://api.windy.com/webcams/api/v3/webcams/1756933961?lang=en&include=images,urls' \
   -H 'accept: application/json' \
-  -H 'x-windy-api-key: m4MBUPLQGbT04Tz6kCxcDIOTbYzfdy6t' | jq -r '.images.current.preview' | xargs curl -o webcam.jpg
+  -H 'x-windy-api-key: m4MBUPLQGbT04Tz6kCxcDIOTbYzfdy6t' | jq -r '.images.current.preview') 
+echo "Downloading webcam image from: $img_url"
+curl $img_url -o webcam.jpg
 
 cp webcam.jpg "$OUTPUT_DIR/webcam.jpg"
+echo "Webcam image saved to: $OUTPUT_DIR/webcam.jpg"
 
 ./update_service_worker.sh $OUTPUT_HTML
